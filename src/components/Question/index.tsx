274f685489b8dd26346fcd3138d0afa8bdecb8ce
Anderson Santos <anderood@hotmail.com>
2023-02-13 21:06:15 -0300

    Criado o component Question

---
+++ src/components/Question/index.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { 
+    Container, 
+    ContainerTitle,
+    Position,
+    Title,
+    ContainerFrame,
+    Description,
+    ContainerOptions,
+    ButtonOption,
+    TextOption,
+    ContainerResponse,
+    DescriptionResponse 
+} from "../Question/styles";
+
+export function Question(){
+    return(
+        <Container>
+            <ContainerTitle>
+                <Position>1 / 8</Position>
+                <Title>Qual é o valor do result?</Title>
+            </ContainerTitle>
+            <ContainerFrame>
+                <Description >
+                    {`
+                import React from 'react';
+                import { View, Text } from 'react-native';
+                
+                const MyComponent = () => {
+                    return (
+                    <View>
+                        <Text>Hello, World!</Text>
+                    </View>
+                    );
+                };
+                
+                export default MyComponent;
+                    `}
+                </Description>
+            </ContainerFrame>
+            <ContainerOptions>
+                <ButtonOption>
+                    <TextOption>throw a SyntaxError</TextOption>
+                </ButtonOption>
+                <ButtonOption>
+                    <TextOption>undefined</TextOption>
+                </ButtonOption>
+                <ButtonOption>
+                    <TextOption>Brendan</TextOption>
+                </ButtonOption>
+                <ButtonOption>
+                    <TextOption>throw a ReferenceError</TextOption>
+                </ButtonOption>
+            </ContainerOptions>
+            <ContainerResponse>
+                <DescriptionResponse>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Cum eligendi dolor velit nobis minima ea soluta, corporis 
+                    facere recusandae voluptatum quam, ex eveniet est unde. 
+                    Ex est molestias sunt laborum!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Cum eligendi dolor velit nobis minima ea soluta, corporis 
+                    facere recusandae voluptatum quam, ex eveniet est unde. 
+                    Ex est molestias sunt laborum!
+                </DescriptionResponse>
+            </ContainerResponse>
+
+        </Container>
+    );
+}
--- src/pages/ViewQuiz/index.tsx
+++ src/pages/ViewQuiz/index.tsx
@@ -1,71 +1,13 @@
 import React from "react";
+import { Question } from "../../components/Question";
 
-import { 
-    Container, 
-    ContainerTitle,
-    Position,
-    Title,
-    ContainerFrame,
-    Description,
-    ContainerOptions,
-    ButtonOption,
-    TextOption,
-    ContainerResponse,
-    DescriptionResponse 
-} from "../ViewQuiz/styles";
+import { Container } from "../ViewQuiz/styles";
 
 export function ViewQuiz(){
     return(
         <Container>
-            <ContainerTitle>
-                <Position>1 / 8</Position>
-                <Title>Qual é o valor do result?</Title>
-            </ContainerTitle>
-            <ContainerFrame>
-                <Description >
-                    {`
-                import React from 'react';
-                import { View, Text } from 'react-native';
-                
-                const MyComponent = () => {
-                    return (
-                    <View>
-                        <Text>Hello, World!</Text>
-                    </View>
-                    );
-                };
-                
-                export default MyComponent;
-                    `}
-                </Description>
-            </ContainerFrame>
-            <ContainerOptions>
-                <ButtonOption>
-                    <TextOption>throw a SyntaxError</TextOption>
-                </ButtonOption>
-                <ButtonOption>
-                    <TextOption>undefined</TextOption>
-                </ButtonOption>
-                <ButtonOption>
-                    <TextOption>Brendan</TextOption>
-                </ButtonOption>
-                <ButtonOption>
-                    <TextOption>throw a ReferenceError</TextOption>
-                </ButtonOption>
-            </ContainerOptions>
-            <ContainerResponse>
-                <DescriptionResponse>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Cum eligendi dolor velit nobis minima ea soluta, corporis 
-                    facere recusandae voluptatum quam, ex eveniet est unde. 
-                    Ex est molestias sunt laborum!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Cum eligendi dolor velit nobis minima ea soluta, corporis 
-                    facere recusandae voluptatum quam, ex eveniet est unde. 
-                    Ex est molestias sunt laborum!
-                </DescriptionResponse>
-            </ContainerResponse>
 
+            <Question></Question>
         </Container>
     );
 }
